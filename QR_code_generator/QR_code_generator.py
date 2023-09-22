f257eb0e2e085647c589da29fd786a5ebe961662
Sahana <sahanaa2602@gmail.com>
2023-09-22 21:43:16 +0530

    commit

--- QR_code_generator/QR_code_generator.py
+++ QR_code_generator/QR_code_generator.py
@@ -1,4 +1,15 @@
-import qrcode as qr
+import qrcode
 
-img = qr.make("https://github.com/Sahana-26")
-img.save("My_github.png")
+qr_code = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=3 )
+
+qr_code.add_data("https://www.linkedin.com/in/sahana26/")
+qr_code.make(fit=True)
+
+img1 = qrcode.make("https://github.com/Sahana-26")
+img1.save("My_github.png")
+img2 = qr_code.make_image(fill_color="red", back_color="blue")
+img2.save("My_linkedIn.png")
